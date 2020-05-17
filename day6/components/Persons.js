ac9c9bcb63739ae86cf5fba45df2e77159906f1a
Samir Omer <samiromer2@yahoo.com>
2020-05-18 01:06:29 +0300

    Update Persons.js

--- day6/components/Persons.js
+++ day6/components/Persons.js
@@ -6,12 +6,16 @@ const Persons    = ({ person , fiterAll }) => {
 	const personsToShow = (fiterAll === '') ? person : person.filter(person1 => person1.name.toLowerCase().includes(fiterAll.toLowerCase()) )
   return (
   <table>
-   
+   <tbody> 
+	<tr>
+    <th>First name</th>
+    <th>Last name</th>
+  </tr>
     
        {personsToShow.map((person) =>
          <Person key={person.id} person={person} />
        )}
-    
+     </tbody>
 		
 	</table>
   )
