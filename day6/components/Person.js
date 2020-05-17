89697de901699bab978a54df61726903e68a2761
Samir Omer <samiromer2@yahoo.com>
2020-05-18 01:06:52 +0300

    Update Person.js

--- day6/components/Person.js
+++ day6/components/Person.js
@@ -1,23 +1,16 @@
 import React from 'react'
-import Person from './Person'
 
-const Persons    = ({ person , fiterAll }) => {
-	
-	const personsToShow = (fiterAll === '') ? person : person.filter(person1 => person1.name.toLowerCase().includes(fiterAll.toLowerCase()) )
+const Person = ({ person }) => {
   return (
-  <table>
-   <tbody> 
-	<tr>
-    <th>First name</th>
-    <th>Last name</th>
-  </tr>
+  
     
-       {personsToShow.map((person) =>
-         <Person key={person.id} person={person} />
-       )}
-     </tbody>
-		
-	</table>
+	<tr><td>
+	{person.name}
+</td>    
+	<td> {person.number} </td></tr>
+	 
+	
   )
 }
-export default Persons
+
+export default Person
